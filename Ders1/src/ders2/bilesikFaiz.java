30553ce25bac1d2ece211fe1cbcdec9f8cb4e88b
Melih <melihkotman@gmail.com>
2024-11-04 23:11:08 +0300

    22 Ekim dersi islendi ve lab yapildi

---
+++ Ders1/src/ders2/bilesikFaiz.java
@@ -0,0 +1,20 @@
+package ders2;
+
+import java.math.*;
+
+public class bilesikFaiz {
+
+	public static void main(String[] args) {
+		double principal = 1000;
+		double rate = 0.05;
+		
+		System.out.printf("%s%20s%n"," Year"," Amonut on deposit");
+		
+		for(int i = 1; i <= 10; i++) {
+			
+			double amount = principal* Math.pow(1 + rate, i);
+			System.out.print(i + "           " + amount + "\n");
+		}
+	}
+
+}
