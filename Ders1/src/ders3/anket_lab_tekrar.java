8c9aa897a849a1952131aed1e736c856755928c0
Melih <melihkotman@gmail.com>
2024-11-11 20:32:35 +0300

    Onceki hafta tekrar edildi ve yapildi

---
+++ Ders1/src/ders3/anket_lab_tekrar.java
@@ -0,0 +1,71 @@
+package ders3;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class anket_lab_tekrar {
+	public static void  main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		HashMap<String, ArrayList<Integer>> sorunlarMap = new HashMap<String,ArrayList<Integer>>();
+		
+		String[] sorunlar = {"Yolsuzluk","Ekonomi","Saglik","Egitim","Tarim"};
+		
+		for(String sorun : sorunlar) {
+			sorunlarMap.put(sorun, new ArrayList<>());
+		}
+		
+		boolean devam = true;
+		
+			while(devam) {
+				System.out.println("Lutfen anketimizi oylayiniz(1-10'a kadar ve 0 yaparsaniz anket kapanir)");
+				
+				for(String sorun : sorunlar) {
+					try {
+					int cevap = -1;
+					
+					while(cevap < 0) {
+					System.out.println(sorun + " sorunu icin oyunuz nedir?");
+					cevap = input.nextInt();
+					
+					if(cevap == 0) {
+						devam = false;
+						break;
+					}
+					
+					else if (cevap < 1 || cevap > 10) {
+	                    System.out.println("Lütfen 1 ile 10 arasında bir puan girin");
+	                    cevap = -1;
+					}
+					
+					else {
+						sorunlarMap.get(sorun).add(cevap);
+					}
+					
+					}
+					if(!devam) {
+						break;
+						}
+			}
+				    catch(InputMismatchException e){
+						System.out.println("Lutfen duzgun degerler giriniz");
+						input.next();
+						
+			}
+			
+			}
+				System.out.println("Anketiniz bitmiştir... Teşekkürler.");
+	}
+			for (String sorun : sorunlar) {
+	            ArrayList<Integer> puanlar = sorunlarMap.get(sorun);
+	            double toplam = 0;
+	            for (int puan : puanlar) {
+	                toplam += puan;
+	            }
+	            double ortalama = toplam / puanlar.size();
+	            System.out.printf("%s sorununun ortalama puanı: %.2f\n", sorun, ortalama);
+	        }
+
+		}
+}
---
+++ Ders1/src/ders3/lab6.java
@@ -0,0 +1,29 @@
+package ders3;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.HashMap;
+public class lab6 {
+
+	public static void main(String[] args) {
+		/**
+		LinkedList<String> car = new LinkedList<String>();
+		car.add("Volvo");
+		car.add("BMW");
+		car.add(0,"Mercedes"); //mercedesi volvo arasina baglanti attik...
+		for(String  ncar : car  ) {
+			System.out.println(ncar);
+		
+		}
+		**/
+		//HASH MAP <key,value> seklinde calisir
+		HashMap <String, Integer> m = new HashMap<String, Integer>();
+		// int yerine Integer yazma sebebimiz Integer bir class'tır ve int yazinca integer sinifindan bilgi cekiyordu simdi sinifi yazdik
+		m.put("Bolu",14 );
+		m.put("Ankara", 6);
+		m.put("Istanbul", 34);
+		for(String i : m.keySet()) { //keySet dedigimiz keyleri alip i'ye attik ve printleyince m.get(i) value'lari aldik
+			System.out.println("Key : " + i + " Value : " + m.get(i));
+		}
+	}
+}
+
