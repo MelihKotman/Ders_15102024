30542203845b09eb01f8abea52b8d6e0e60641b0
Melih <melihkotman@gmail.com>
2024-11-19 19:29:44 +0300

    Bu haftaki lab ve ders yapildi(kalitim polymorphism super fonk. class.)

---
+++ Ders1/src/Lab/Person.java
@@ -0,0 +1,5 @@
+package Lab;
+
+public interface Person {
+	public void Gender(String c);
+}
---
+++ Ders1/src/Lab2/Fish.java
@@ -0,0 +1,24 @@
+package Lab2;
+
+public class Fish extends Animal implements Pet {
+	String name;
+	Fish(String name,int legs){
+		super(legs);
+		setLegs(0);
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void play() {
+		System.out.println(getName() + "balik oynamadi... olmus\n");
+	}
+	public void walk() {
+		System.out.println("Balik yurumez...\n");
+	}
+	public void eat() {
+		System.out.println(getName() + "yemini yedi...\n");
+	}
+}
